8d7b486185550f4a9148316ce9a37f29baed6c57
vatty0412 <vatty041206@gmail.com>
2025-10-04 14:49:59 +0530

    Add startup logos and refactor Startups component to use dynamic data

---
+++ client/src/assets/startups/index.js
@@ -0,0 +1,66 @@
+import baabtra from "./baabtra.png";
+import loco from "./loco.png";
+import drivezy from "./drivezy.png";
+import unacademy from "./unacademy.png";
+import pvr from "./PVR.png";
+import everyCrave from "./EveryCrave.png";
+import kratikal from "./kratikal.png";
+import symbTechnologies from "./SymbTechnologies.png";
+import trelos from "./Trelos.png";
+
+export const startups = [
+    {
+        name: "Baabtra",
+        link: "https://baabtra.com/",
+        alt: "baabtra.com",
+        logo: baabtra
+    },
+    {
+        name: "Loco",
+        link: "https://loco.gg/",
+        alt: "loco.gg",
+        logo: loco
+    },
+    {
+        name: "Drivezy",
+        link: "https://drivezy.com/",
+        alt: "drivezy.com",
+        logo: drivezy
+    },
+    {
+        name: "Unacademy",
+        link: "https://unacademy.com/",
+        alt: "unacademy.com",
+        logo: unacademy
+    },
+    {
+        name: "PVR",
+        link: "https://www.pvrcinemas.com/",
+        alt: "pvrcinemas.com",
+        logo: pvr
+    },
+    {
+        name: "EveryCrave",
+        link: "http://everycrave.me/",
+        alt: "everycrave.me",
+        logo: everyCrave
+    },
+    {
+        name: "Kratikal",
+        link: "https://kratikal.com/",
+        alt: "kratikal.com",
+        logo: kratikal
+    },
+    {
+        name: "Symb Technologies",
+        link: "https://www.symbtechnologies.com/",
+        alt: "symbtechnologies.com",
+        logo: symbTechnologies
+    },
+    {
+        name: "Trelos",
+        link: "https://www.trelos.in/",
+        alt: "trelos.in",
+        logo: trelos
+    },
+]
--- client/src/components/Startups/Startups.jsx
+++ client/src/components/Startups/Startups.jsx
@@ -1,17 +1,8 @@
-import logo1 from "./Baabtra.png";
-import logo2 from "./Loco.jpg";
-import logo3 from "./Drivezy.png";
-import logo4 from "./EveryCrave.jpg";
-import logo5 from "./Kratikal.png";
-import logo6 from "./SymbTechnologies.png";
-import logo7 from "./Trelos.png";
-import logo8 from "./Unacademy.png";
-import logo9 from "./PVR.jpg";
+import { startups } from "../../assets/startups";
 import { motion } from "framer-motion";
 const Startups = () => {
   return (
-    <div className="mt-12 mb-8" id="startups">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center xl:py-20 xl:pb-16 sm:py-10" id="startups">
         <div id="stick" className="flex justify-center">
           <div className="bg-stone-400 h-20 w-1 mb-8"></div>
         </div>
@@ -25,147 +16,28 @@ const Startups = () => {
             STARTUPS BY MNNITians
           </div>
         </motion.div>
-        <div className="flex space-x-6 justify-center w-full items-center mt-10 z-0">
-          <motion.div
-            whileInView={{ x: [-20, 20], opacity: [0, 1] }}
-            transition={{ duration: 1, ease: "easeOut" }}
-            whileHover={{ scale: 1.1 }}
-            className="bg-white rounded-2xl"
-          >
-            <a target="_blank" rel="noreferrer" href="https://baabtra.com/">
-              <img
-                className="object-contain w-40 h-40 rounded-2xl"
-                src={logo1}
-                alt="baabtra.com"
-              />
-            </a>
-          </motion.div>
-          <motion.div
-            whileInView={{ x: [-20, 20], opacity: [0, 1] }}
-            transition={{ duration: 1, ease: "easeOut" }}
-            whileHover={{ scale: 1.1 }}
-          >
-            <a target="_blank" rel="noreferrer" href="https://loco.gg/">
-              {" "}
-              <img
-                className="object-contain w-40 h-40 rounded-2xl"
-                src={logo2}
-                alt="loco.com"
-              />
-            </a>
-          </motion.div>
-          <motion.div
-            whileInView={{ x: [-20, 20], opacity: [0, 1] }}
-            transition={{ duration: 1, ease: "easeOut" }}
-            whileHover={{ scale: 1.1 }}
-            className="bg-white rounded-2xl"
-          >
-            <a
+        <div className="flex flex-wrap justify-center">
+          {startups.map((startup, index) => (
+            <motion.a
+              key={index}
+              whileInView={{ x: [-20, 0], opacity: [0, 1] }}
+              transition={{ duration: 1, ease: "easeOut" }}
+              whileHover={{ scale: 1.1 }}
+              href={startup.link}
               target="_blank"
-              rel="noreferrer"
-              href="https://drivezy.com/Bengaluru"
+              className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 flex justify-center items-center space-y-5 py-5 rounded-2xl"
             >
-              <img
-                className="object-contain w-40 h-40 rounded-2xl"
-                src={logo3}
-                alt="drivezy.com"
-              />
-            </a>
-          </motion.div>
-          <motion.div
-            whileInView={{ x: [-20, 20], opacity: [0, 1] }}
-            transition={{ duration: 1, ease: "easeOut" }}
-            whileHover={{ scale: 1.1 }}
-          >
-            <a target="_blank" rel="noreferrer" href="http://everycrave.me/">
-              <img
-                className="object-contain w-40 h-40 rounded-2xl"
-                src={logo4}
-                alt="EveryCrave.com"
-              />
-            </a>
-          </motion.div>
-          <motion.div
-            whileInView={{ x: [-20, 20], opacity: [0, 1] }}
-            transition={{ duration: 1, ease: "easeOut" }}
-            whileHover={{ scale: 1.1 }}
-          >
-            <a target="_blank" rel="noreferrer" href="https://kratikal.com/">
-              <img
-                className="object-contain w-40 h-40 rounded-2xl"
-                src={logo5}
-                alt="kratikal.com"
-              />
-            </a>
-          </motion.div>
-        </div>
-        <div className="flex space-x-6 justify-center w-full items-center mt-6">
-          <motion.div
-            whileInView={{ x: [-20, 20], opacity: [0, 1] }}
-            transition={{ duration: 1, ease: "easeOut" }}
-            whileHover={{ scale: 1.1 }}
-          >
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.symbtechnologies.com/"
-            >
-              <img
-                className="object-contain w-40 h-40 rounded-2xl"
-                src={logo6}
-                alt="symb.com"
-              />
-            </a>
-          </motion.div>
-          <motion.div
-            whileInView={{ x: [-20, 20], opacity: [0, 1] }}
-            transition={{ duration: 1, ease: "easeOut" }}
-            whileHover={{ scale: 1.1 }}
-            className="bg-white rounded-2xl"
-          >
-            <a target="_blank" rel="noreferrer" href="https://www.trelos.in/">
-              <img
-                className="object-contain w-40 h-40 rounded-2xl"
-                src={logo7}
-                alt="trelos.com"
-              />
-            </a>
-          </motion.div>
-          <motion.div
-            whileInView={{ x: [-20, 20], opacity: [0, 1] }}
-            transition={{ duration: 1, ease: "easeOut" }}
-            whileHover={{ scale: 1.1 }}
-            className=""
-          >
-            <a target="_blank" rel="noreferrer" href="https://unacademy.com/">
-              <img
-                className="object-contain w-40 h-40 rounded-2xl"
-                src={logo8}
-                alt="unacademy.com"
-              />
-            </a>
-          </motion.div>
-          <motion.div
-            whileInView={{ x: [-20, 20], opacity: [0, 1] }}
-            transition={{ duration: 1, ease: "easeOut" }}
-            whileHover={{ scale: 1.1 }}
-            className="bg-white rounded-2xl"
-          >
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.pvrcinemas.com/"
-            >
-              <img
-                className="object-contain w-40 h-40 rounded-2xl"
-                src={logo9}
-                alt="pvr.com"
-              />
-            </a>
-          </motion.div>
+              <div className="bg-orange-100 h-fit w-fit rounded-full flex justify-center items-center">
+                <img
+                  className="object-contain w-40 h-40 rounded-2xl"
+                  src={startup.logo}
+                  alt={startup.alt}
+                />
+              </div>
+            </motion.a>
+          ))}
         </div>
       </div>
-    </div>
   );
 };
 
