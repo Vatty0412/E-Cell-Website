1f68aa3249765c835dd87191d4f07c70e2d2e706
vatty0412 <vatty041206@gmail.com>
2025-10-05 03:52:27 +0530

    add new startup logos, remove outdated images, and implement a marquee display for better presentation

--- client/src/assets/startups/index.js
+++ client/src/assets/startups/index.js
@@ -7,60 +7,136 @@ import everyCrave from "./EveryCrave.png";
 import kratikal from "./kratikal.png";
 import symbTechnologies from "./SymbTechnologies.png";
 import trelos from "./Trelos.png";
+import nobero from "./nobero.png";
+import tsaw from "./Tsaw.png";
+import dot from "./dot.png";
+import wheelsOnRent from "./wheelsOnRent.png";
+import dunzo from "./dunzo.png";
+import cograd from "./cograd.png";
+import emergent from "./emergent.png";
+import cube26 from "./cube26.png";
+import meritRoad from "./meritroad.png";
+import auxia from "./auxia.svg";
+import droneco from "./droneco.png";
+import getWork from "./getWork.png";
+import bookRento from "./bookRento.png";
+import nostaviaHealth from "./nostaviaHealth.png";
 
+// TODO: Arrange for SVGs instead of PNGs wherever possible for better quality and smaller size
 export const startups = [
     {
-        name: "Baabtra",
+        alt: "Baabtra",
         link: "https://baabtra.com/",
-        alt: "baabtra.com",
         logo: baabtra
     },
     {
-        name: "Loco",
+        alt: "Loco",
         link: "https://loco.gg/",
-        alt: "loco.gg",
         logo: loco
     },
     {
-        name: "Drivezy",
+        alt: "Drivezy",
         link: "https://drivezy.com/",
-        alt: "drivezy.com",
         logo: drivezy
     },
     {
-        name: "Unacademy",
+        alt: "Unacademy",
         link: "https://unacademy.com/",
-        alt: "unacademy.com",
         logo: unacademy
     },
     {
-        name: "PVR",
+        alt: "PVR",
         link: "https://www.pvrcinemas.com/",
-        alt: "pvrcinemas.com",
         logo: pvr
     },
     {
-        name: "EveryCrave",
+        alt: "EveryCrave",
         link: "http://everycrave.me/",
-        alt: "everycrave.me",
         logo: everyCrave
     },
     {
-        name: "Kratikal",
+        alt: "Kratikal",
         link: "https://kratikal.com/",
-        alt: "kratikal.com",
         logo: kratikal
     },
     {
-        name: "Symb Technologies",
+        alt: "Symb Technologies",
         link: "https://www.symbtechnologies.com/",
-        alt: "symbtechnologies.com",
         logo: symbTechnologies
     },
     {
-        name: "Trelos",
+        alt: "Trelos",
         link: "https://www.trelos.in/",
-        alt: "trelos.in",
         logo: trelos
     },
+    {
+        alt: "Nobero",
+        link: "https://nobero.com/",
+        logo: nobero
+    },
+    {
+        alt: "Tsaw",
+        link: "https://www.tsaw.tech/",
+        logo: tsaw
+    },
+    {
+        alt: "Dot",
+        link: "https://www.dot.news/home",
+        logo: dot
+    },
+    {
+        alt: "Wheels On Rent",
+        logo: wheelsOnRent,
+        link: "https://wheelsonrentallahabad.wordpress.com/" // TODO: check with seniors for this
+    },
+    {
+        alt: "Dunzo",
+        logo: dunzo,
+        link: "https://www.dunzo.in/" // TODO: check with seniors for this
+    },
+    {
+        alt: "Cograd",
+        logo: cograd,
+        link: "https://cograd.in/"
+    },
+    {
+        alt: "Emergent",
+        logo: emergent,
+        link: "https://www.emergentbiosolutions.com/"
+    },
+    {
+        alt: "Cube26",
+        logo: cube26,
+        link: "https://www.cube26.com/" // TODO: check with seniors for this
+    },
+    {
+        alt: "MeritRoad",
+        logo: meritRoad,
+        link: "https://www.linkedin.com/company/meritroad-private-limited/?originalSubdomain=in" // TODO: find original link for website
+    },
+    {
+        alt: "Auxia",
+        logo: auxia,
+        link: "https://www.auxia.io/en/"
+    },
+    {
+        alt: "Droneco",
+        logo: droneco,
+        link: "https://godroneco.com/"
+    },
+    {
+        alt: "GetWork",
+        logo: getWork,
+        link: "https://www.getwork.org/"
+    },
+    {
+        alt: "BookRento",
+        logo: bookRento,
+        link: "https://x.com/bmnnit" // TODO: check with seniors for this
+    },
+    {
+        alt: "NostaviaHealth",
+        logo: nostaviaHealth,
+        link: "https://www.nostaviahealth.com/"
+    }
 ]
--- client/src/components/Startups/Startups.jsx
+++ client/src/components/Startups/Startups.jsx
@@ -1,44 +1,87 @@
-import { startups } from "../../assets/startups";
-import { motion } from "framer-motion";
-const Startups = () => {
-  return (
-      <div className="flex flex-col items-center xl:py-20 xl:pb-16 sm:py-10" id="startups">
-        <div id="stick" className="flex justify-center">
-          <div className="bg-stone-400 h-20 w-1 mb-8"></div>
-        </div>
+import { useRef, useEffect, useState } from 'react';
+import { startups } from '../../assets/startups';
+import { motion } from 'framer-motion';
+
+const Marquee = ({ items, direction, pixelsPerSecond }) => {
+    const marqueeRef = useRef(null);
+    const [marqueeWidth, setMarqueeWidth] = useState(0);
+    const duplicatedItems = [...items, ...items];
+    
+    useEffect(() => { if (marqueeRef.current) setMarqueeWidth(marqueeRef.current.scrollWidth / 2); }, []);
+    const duration = marqueeWidth / pixelsPerSecond;
+
+    return (
         <motion.div
-          whileInView={{ y: [50, 0], opacity: [0, 1] }}
-          transition={{ duration: 2, ease: "easeOut" }}
-          whileTap={{ scale: 0.9 }}
-          className="text-4xl text-stone-300 flex flex-col items-center mb-4"
+            ref={marqueeRef}
+            className="flex flex-nowrap gap-16 justify-start py-5"
+            animate={{
+                x: direction === 'left' ? [0, -marqueeWidth] : [-marqueeWidth, 0],
+            }}
+            transition={{
+                x: {
+                    repeat: Infinity,
+                    repeatType: 'loop',
+                    duration: duration,
+                    ease: 'linear',
+                },
+            }}
         >
-          <div className="text-4xl text-center text-stone-300">
-            STARTUPS BY MNNITians
-          </div>
+            {duplicatedItems.map((startup, index) => (
+                <motion.a
+                    key={index}
+                    whileHover={{
+                        scale: [1, 1.05, 0.98, 1.02, 1],
+                        transition: { duration: 0.6 },
+                    }}
+                    href={startup.link}
+                    target="_blank"
+                >
+                    <div className="bg-orange-100 h-40 w-40 rounded-full flex justify-center items-center p-3">
+                        <img
+                            className="object-contain max-w-full max-h-full"
+                            src={startup.logo}
+                            alt={startup.alt}
+                        />
+                    </div>
+                </motion.a>
+            ))}
         </motion.div>
-        <div className="flex flex-wrap justify-center">
-          {startups.map((startup, index) => (
-            <motion.a
-              key={index}
-              whileInView={{ x: [-20, 0], opacity: [0, 1] }}
-              transition={{ duration: 1, ease: "easeOut" }}
-              whileHover={{ scale: 1.1 }}
-              href={startup.link}
-              target="_blank"
-              className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 flex justify-center items-center space-y-5 py-5 rounded-2xl"
+    );
+};
+
+
+const Startups = () => {
+    const firstRow = startups.slice(0, Math.ceil(startups.length / 2));
+    const secondRow = startups.slice(Math.ceil(startups.length / 2));
+
+    return (
+        <motion.div
+            className="flex flex-col items-center xl:py-20 xl:pb-16 sm:py-10" id="startups"
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5, ease: 'easeInOut' }}
+        >
+            <motion.div
+                whileInView={{ y: [50, 0], opacity: [0, 1] }}
+                transition={{ duration: 2, ease: 'easeOut' }}
+                className="text-4xl text-stone-300 flex flex-col items-center mb-4"
             >
-              <div className="bg-orange-100 h-fit w-fit rounded-full flex justify-center items-center">
-                <img
-                  className="object-contain w-40 h-40 rounded-2xl"
-                  src={startup.logo}
-                  alt={startup.alt}
-                />
-              </div>
-            </motion.a>
-          ))}
-        </div>
-      </div>
-  );
+                <div className="text-4xl text-center text-stone-300">STARTUPS BY MNNITians</div>
+            </motion.div>
+
+            <div id="stick" className="flex justify-center">
+                <div className="bg-stone-400 w-80 h-1 my-10"></div>
+            </div>
+
+            <div className="w-full overflow-hidden flex flex-col space-y-10">
+                <Marquee items={ firstRow } direction="left" pixelsPerSecond={ 75 } />
+                <Marquee items={ secondRow } direction="right" pixelsPerSecond={ 75 } />
+            </div>
+            
+            <div id="stick" className="flex justify-center">
+                <div className="bg-stone-400 w-80 h-1 my-10"></div>
+            </div>
+        </motion.div>
+    );
 };
 
 export default Startups;
